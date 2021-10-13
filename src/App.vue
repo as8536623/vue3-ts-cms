fa98be2bb784f1d830a7fdcca9a4f6505ab3c92b
dingshuai <624677633@qq.com>
2021-10-13 13:43:21 +0800

    feat(app): dingshuai

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    <span>appdddff</span>
+    <span>g</span>
   </div>
   <router-view />
 </template>
