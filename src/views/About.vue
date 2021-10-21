5c3a069cc55a4959074b2259d3b494757f34ef92
dingshuai <624677633@qq.com>
2021-10-21 14:28:47 +0800

    feat(vue.config.js): dingshuai

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about pagedd</h1>
+    <login></login>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import login from 'components/login.vue'
+
+export default defineComponent({
+  name: 'Home',
+  components: { login }
+})
+</script>
